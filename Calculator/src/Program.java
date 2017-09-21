02227147ee99022b22c582c97849169129ec8193
Osama Kawish <osamakawish@users.noreply.github.com>
2017-09-21 10:13:25 -0400

    Update Program.java

--- Calculator/src/Program.java
+++ Calculator/src/Program.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
 		// To interpret inputs and provide output.
 		Interpretor I = new Interpretor();
